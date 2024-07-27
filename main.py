7500bb7a4e1750902f1f9a25bbb7a108bfc0ed18
riain <ryanftoal@gmail.com>
2024-07-27 01:39:56 +0100

    Added overlap checker tool

--- main.py
+++ main.py
@@ -27,6 +27,11 @@ class Time:
     def __repr__(self) -> str:
         return f'{self._time.hour:02}:{self._time.minute:02}:{self._time.second:02},{self._time.microsecond:03}'
 
+    @property
+    def microseconds(self) -> int:
+        return (self._time.hour * 3600000000 + self._time.minute * 60000000 +
+                self._time.second * 1000000 + self._time.microsecond)
+
     def modify_time(self, amount: float) -> None:
         if amount >= 0:
             t = timedelta(seconds=amount)
@@ -78,8 +83,12 @@ def within_bounds(begin: str, bounds: tuple) -> bool:
     return all([time >= bounds[0], time <= bounds[1]])
 
 
+def is_timestamp(line: str) -> bool:
+    return bool(re.match(r'^\d{2}:\d{2}:\d{2},\d{3} --> \d{2}:\d{2}:\d{2},\d{3}$', line))
+
+
 def fix_synchronisation(line: str, amount: float, bounds: tuple) -> str:
-    if not re.findall(r'\d:\d', line):
+    if not is_timestamp(line):
         return line
 
     if not within_bounds(line, bounds):
@@ -107,19 +116,9 @@ def valid_timestamp(arg: str) -> bool:
     return bool(re.match(r'^\d+:[0-5][0-9]:[0-5][0-9]($|.\d+$)', arg))
 
 
-def main() -> None:
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-b', '--begin', type=str, default='00:00:00,000000')
-    parser.add_argument('-e', '--end', type=str, default='23:59:59,999999')
-    parser.add_argument('-f', '--filename', type=str)
-    parser.add_argument('-o', '--output', type=str)
-    parser.add_argument('-a', '--amount', type=float)
-
-    args = parser.parse_args()
+def resync_subs(args) -> None:
     assert valid_timestamp(args.begin), f'Invalid timestamp: {args.begin}'
     assert valid_timestamp(args.end), f'Invalid timestamp: {args.end}'
-    assert Path(args.filename).is_file(), f'File not found: {args.filename}'
-    assert Path(args.filename).suffix == '.srt', f'Not .srt file: {args.filename}'
     assert args.amount, 'No amount entered'
 
     read = Path(args.filename)
@@ -130,9 +129,56 @@ def main() -> None:
         write = Path(f'{read.stem}_copy{read.suffix}')
 
     with open(write, 'w') as file:
-        for text in read_file(read):
-            text = fix_synchronisation(text, -3, (Time(args.begin), Time(args.end)))
-            file.write(text)
+        for line in read_file(read):
+            line = fix_synchronisation(line, -3, (Time(args.begin), Time(args.end)))
+            file.write(line)
+
+    return
+
+
+def find_overlap(args) -> None:
+    read = Path(args.filename)
+
+    timestamps: list[tuple[Time, ...]] = []
+
+    for line in read_file(read):
+        if not is_timestamp(line):
+            continue
+        timestamps.append(tuple(map(Time, line.split('-->'))))
+
+    for i in range(len(timestamps[:-1])):
+        begin = timestamps[i][0]
+        end = timestamps[i][1]
+
+        if begin.microseconds > end.microseconds:
+            print(f'{begin} overlaps with {end}')
+
+        end = timestamps[i][1]
+        begin = timestamps[i+1][0]
+
+        if end.microseconds > begin.microseconds:
+            print(f'{end} overlaps with {begin}')
+
+    return
+
+
+def main() -> None:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-t', '--tool', type=str, default='resync')
+    parser.add_argument('-b', '--begin', type=str, default='00:00:00,000000')
+    parser.add_argument('-e', '--end', type=str, default='23:59:59,999999')
+    parser.add_argument('-f', '--filename', type=str)
+    parser.add_argument('-o', '--output', type=str)
+    parser.add_argument('-a', '--amount', type=float)
+    args = parser.parse_args()
+
+    assert Path(args.filename).is_file(), f'File not found: {args.filename}'
+    assert Path(args.filename).suffix == '.srt', f'Not .srt file: {args.filename}'
+
+    if args.tool == 'resync':
+        resync_subs(args)
+    elif args.tool == 'overlap':
+        find_overlap(args)
 
     return
 
